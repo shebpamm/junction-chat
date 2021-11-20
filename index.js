3fdd70682732dcfe74577ec5d85b54d229886123
Erik Karsten <erik@opcode.xyz>
2021-11-21 00:11:07 +0200

    Add config file require

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 const axios = require('axios');
+const config = require('./config.json');
 
 
 const APP_PORT = process.env.PORT || 8080;
