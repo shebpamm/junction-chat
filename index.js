c0078259fb127bb6211b521a1d44ce8929944486
Erik Karsten <erik@opcode.xyz>
2021-11-20 22:54:41 +0200

    New functions for post and get, emit for every message when connecting

--- index.js
+++ index.js
@@ -4,24 +4,39 @@ const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
+const axios = require('axios');
+
 
 const APP_PORT = process.env.PORT || 8080;
 
 app.use('/', express.static(__dirname + '/front'));
 
 function publishMessage(message) {
-    
+    return axios.post(
+        'https://us-central1-junction-2021-fee21.cloudfunctions.net/webApi/api/v1/message',
+         message);
 }
 
-io.on('connection', (socket) => {
+function getMessages() {
+    return axios.get(
+        'https://us-central1-junction-2021-fee21.cloudfunctions.net/webApi/api/v1/messages');
+}
+
+io.on('connection', async (socket) => {
     console.log('a user connected');
+
+    const messages = await getMessages();
+    for (const message of messages.data) {
+        socket.emit('chat message', message);
+    }
+
     socket.on('disconnect', () => {
         console.log('user disconnected');
         });
 
-    socket.on('chat message', (msg) => {
-        data = publishMessage(msg);
-        io.emit('chat message', msg);
+    socket.on('chat message', async (msg) => {
+        data = await publishMessage(msg);
+        io.emit('chat message', data.data);
         console.log(msg.username + ': ' + msg.message);
     });
 });
